9e698c7f612adb8480df9b2f00d87144bbed4128
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2017-04-22 20:05:04 -0700

    README and tests

---
+++ README.md
@@ -0,0 +1,41 @@
+# check checksum
+
+I needed this same function for two things, so I'm using this to play with 
+writing module in es6/es2015.
+
+## install
+
+
+## test
+
+Runs test and generates code coverage report.
+```
+npm test
+```
+
+## example
+
+
+```es6
+import check_checksum from 'check_checksum';
+
+```
+
+
+## handy when coding 
+```
+npm link .
+```
+
+## links
+
+I was trying to follow along with the examples here:
+
+https://onsen.io/blog/mocha-chaijs-unit-test-coverage-es6/ -- some of this is out of date
+
+https://github.com/domenic/chai-as-promised
+
+https://booker.codes/how-to-build-and-publish-es6-npm-modules-today-with-babel/
+
+http://stackoverflow.com/a/33608835 -- “unexpected token import” in Nodejs5 and babel?
+
--- test/check_checksum.spec.js
+++ test/check_checksum.spec.js
@@ -3,5 +3,29 @@ import check_checksum from '../src/check_checksum';
 var chai = require("chai");
 var chaiAsPromised = require("chai-as-promised");
 
-
 chai.use(chaiAsPromised);
+
+chai.should();
+
+describe('check_checksum', () => {
+  const testsum = 'd3b07384d113edec49eaa6238ad5ff00';
+  describe('#check_checksum()', () => {
+
+    it('should match here', () => {
+      return check_checksum(`${__dirname}/data-good/${testsum}`, testsum)
+        .should.eventually.equal(true);
+    });
+
+    it('should not match here', () => {
+      return check_checksum(`${__dirname}/data-bad/${testsum}`, testsum)
+        .should.be.rejectedWith('checksum mismatch: found db5f554e153ae6d01b400e2f6edb68af expected d3b07384d113edec49eaa6238ad5ff00')
+    });
+
+    it('should fail when file does not exist', () => {
+      return check_checksum(`${__dirname}/data-not-found/${testsum}`, testsum)
+        .should.be.rejectedWith(Error);
+    });
+  });
+  describe('command line wrapper', () => {
+  });
+});
